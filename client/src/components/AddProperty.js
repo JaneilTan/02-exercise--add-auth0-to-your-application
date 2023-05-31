04830918cc9e4c04f2efbfef630b5583099968dd
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-06-01 10:57:57 +1200

    Import useAuth0

--- client/src/components/AddProperty.js
+++ client/src/components/AddProperty.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 import "./AddProperty.css";
 
 const Create = () => {
@@ -12,10 +13,11 @@ const Create = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   const [errorStatus, setErrorStatus] = useState(false);
+  const { getAccessTokenSilently } = useAuth0();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-
+    const accessToken = await getAccessTokenSilently();
     setIsLoading(true);
 
     const property = {
@@ -30,6 +32,7 @@ const Create = () => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
       },
       body: JSON.stringify(property),
     });
